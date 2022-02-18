2a03c494bac01c7e1241e3b56bcd988384e7df24
Wajeeha Jamil <wajeeha.jamil@tuni.fi>
2022-02-18 15:45:04 +0200

    Updated Files

--- dataset/GTZANDataset.py
+++ dataset/GTZANDataset.py
@@ -1,9 +1,23 @@
+from pickle import load as pickle_load
+from pathlib import Path
+from sklearn.datasets import load_files
+import torch
+import numpy as np
 
 class GTZANDataset:
     def __init__(self, df):
-        self.x_paths = df['paths']
-        self.labels = df['target']
+        self.paths = df['feature_path']
+
+    def load_file(self,file_path):
+        with file_path.open('rb') as f:
+            dict = pickle_load(f)
+        return dict['features'], dict['class']
+
+    def __len__(self):
+        return len(self.paths)
 
     def __getitem__(self, idx):
-        x = lb.load(x_paths[idx])
-        y = labels[idx]
+       path = self.paths[idx]
+       features, target = self.load_file(Path(path))
+       target = np.array(target.split(',')).astype('float')
+       return torch.tensor(features), torch.tensor(target)
--- main.py
+++ main.py
@@ -1,47 +1,41 @@
+from re import T
 import torch
 import numpy as np
 import librosa as lb
 from utils import *
 from dataset.GTZANDataset import GTZANDataset
-from serialize_data.serialize_data import serialize_data
+from serialize_data.serialize_data import serialize_data,prepare_datasets
 from pickle import load as pickle_load
-from path import Path
+#from path import Path
+import glob
 
 def main(cfg):
-    df = load_csv(cfg['paths']['csv_path'])
-    print(df)
-    #sound = lb.load(df.iloc[0][0])
-    #print(sound)
-    #print(df.iloc[0][0])
+    train_df = load_csv(cfg['paths']['train_csv_path'])
+    test_df = load_csv(cfg['paths']['test_csv_path'])
+
     for fold in range(5):
-        test_df = df[df['fold'] == fold]
-        train_df = df[df['fold'] != fold]
-    
-    #test_df = df[df['fold'] == 0]
-    #print(test_df)
-    #train_df = df[df['fold'] != 0]
-    #print(train_df)
+        print(f'Starting Fold: {fold + 1}')
+        df_val = train_df[train_df['fold'] == fold].drop(columns = ['fold'])
+        df_train = train_df[train_df['fold'] != fold].drop(columns = ['fold'])
 
-    #training_dataset = GTZANDataset(train_df)
-    #testing_dataset = GTZANDataset(test_df)
 
-def test(cfg):
-    df = load_csv(cfg['paths']['train_csv_path'])
-    file_path = Path(df.iloc[0]['feature_path'])
-    with file_path.open('rb') as f:
-            dict = pickle_load(f)
-    x = dict['features']
-    y = dict['class'].split(',')
-    print(np.shape(x))
-    print(np.shape(y))
-    print(y)
-    
+        train_dataset = GTZANDataset(df_train)
+        val_dataset = GTZANDataset(df_val)
+
+        #dataloaders
+
+        #accuracy for validation and training accuracy
+
+        #accuracy for testing
+
+        
+
     
 if __name__ == '__main__':
     cfg = read_yaml()
-    
     if cfg['defaults']['serialize_data']:
         serialize_data(cfg)
-        
-    test(cfg)
-    #main(cfg)
+
+    #test(cfg)  
+    #test_2(cfg)
+    main(cfg)   

548d4d2ebc460f6851e6a4bdf7297f98f4b93009
Umar <48470589+UmarZubair@users.noreply.github.com>
2022-02-25 15:30:51 +0200

    Updated files

--- main.py
+++ main.py
@@ -1,46 +1,38 @@
-from pickletools import optimize
 import torch
 import numpy as np
 from utils import *
 from model.net import Net
 from dataset.GTZANDataset import GTZANDataset
-from serialize_data.serialize_data import serialize_data,prepare_datasets
-from pickle import load as pickle_load
+from serialize_data.serialize_data import serialize_data
 from torch.utils.data import DataLoader
-from serialize_data.feature_extract import extract_mel_band_energies
 from tqdm import tqdm
 
-def main(cfg):
+
+def main(cfg, device):
+    print('\nStarting training!\n')
     train_df = load_csv(cfg['paths']['train_csv_path'])
-    test_df = load_csv(cfg['paths']['test_csv_path'])
-    test_dataset = GTZANDataset(test_df)
-    test_dataloader = DataLoader(test_dataset,
-                                    batch_size=cfg['train']['batch_size'], 
-                                    shuffle=True)
-        
-    for fold in range(1):
+
+    for fold in range(cfg['folds']['train_folds']):
         print('\nStarting Fold: {}'.format(fold + 1))
         df_val = train_df[train_df['fold'] == fold].drop(columns = ['fold'])
         df_train = train_df[train_df['fold'] != fold].drop(columns = ['fold'])
 
         train_dataset = GTZANDataset(df_train)
         val_dataset = GTZANDataset(df_val)
-        
         train_dataloader = DataLoader(train_dataset, 
                                     batch_size=cfg['train']['batch_size'], 
                                     shuffle=True)
         
         val_dataloader = DataLoader(val_dataset,
                                     batch_size=cfg['train']['batch_size'], 
-                                    shuffle=False)
+                                    shuffle=True)
         
-        device = cfg['defaults']['device']
-        print('\nDevice on: {}'.format(device))
         net = Net()
         net.to(device).train()
         optimizer = torch.optim.Adam(net.parameters(),
-                                     lr= cfg['train']['lr'])
+                                     lr= float(cfg['train']['lr']))
         criterion = torch.nn.CrossEntropyLoss()
+        lowest_val_loss = float(cfg['train']['lowest_val_loss'])
         
         for epoch in range(cfg['train']['epochs']):
             print('\nEpoch: {}\n'.format(epoch + 1))
@@ -50,7 +42,21 @@ def main(cfg):
                                                                                                       train_accuracy, 
                                                                                                       val_loss,
                                                                                                       val_accuracy))
-            
+            # Check early stopping conditions.
+            if val_loss < lowest_val_loss:
+                lowest_val_loss = val_loss
+                patience_counter = 0
+                print('Val loss improved. Saving weights')
+                loss = np.round(np.array(val_loss),3)
+                best_path = cfg['paths']['weights_path'] + f"fold_{fold}_{loss}_model.pth"
+                torch.save(net.state_dict(), best_path)
+            else:
+                patience_counter += 1
+                if patience_counter > cfg['train']['patience']:
+                    print('Early stopping. Exitting training. ')
+                    break   
+    return best_path
+
 def train_one_epoch(data_loader, optimizer, criterion, net, device):
     running_loss = 0
     dataset_size = 0
@@ -106,6 +112,46 @@ def validate_one_epoch(data_loader, criterion, net, device):
     return epoch_loss, epoch_accuracy
             
         
+def evaluate(cfg, weights_path, device):
+    print('\nStarting evaluating!\n')
+    net = Net()
+    net.to(device)
+    net.load_state_dict(torch.load(weights_path))
+    net.eval()
+    criterion = torch.nn.CrossEntropyLoss()
+    
+    test_df = load_csv(cfg['paths']['test_csv_path'])
+    test_dataset = GTZANDataset(test_df)
+    test_dataloader = DataLoader(test_dataset,
+                                    batch_size=cfg['train']['batch_size'], 
+                                    shuffle=False)
+    running_loss = 0
+    preds = []
+    targets = []
+    correct = 0
+    with torch.no_grad(): 
+        for batch_idx, (data, target) in tqdm(enumerate(test_dataloader), total=len(test_dataloader)):
+            if device != 'cpu':
+                data = data.to(device)
+                target = target.to(device)
+            
+            batch_size = data.size(0)
+            pred = net(data.unsqueeze(1))
+            loss = criterion(pred, target)
+            
+            preds.append(pred.view(-1).cpu().detach().numpy())
+            targets.append(target.view(-1).cpu().detach().numpy())
+            running_loss += loss.item() * batch_size
+            predicted = torch.argmax(pred, 1)
+            correct += predicted.eq(torch.argmax(target, 1)).sum().item()
+
+    targets = np.concatenate(targets)
+    preds = np.concatenate(preds)
+    epoch_accuracy = np.round(np.array(100 * correct / len(test_dataloader.dataset)),3)   
+    epoch_loss = running_loss / len(test_dataloader.dataset)
+    print('\nTest accuracy: {}\t Test loss: {}'.format(epoch_accuracy, epoch_loss))
+
+    
 def testing():
     #audio,sr = get_audio_file_data('data/audio/classical/classical.00042.au')
     #spec = extract_mel_band_energies(audio)
@@ -124,11 +170,19 @@ def testing():
     
 if __name__ == '__main__':
     cfg = read_yaml()
-         
+    device = cfg['defaults']['device']
+    print('\nDevice on: {}'.format(device))
+    best_weight_path = cfg['paths']['best_weight_path']
+    
     if cfg['defaults']['serialize_data']:
         clear_feature_folders(cfg)
         serialize_data(cfg)
-    main( cfg)
+        
+    if cfg['defaults']['train']:
+        best_weight_path = main(cfg, device)
+    
+    if cfg['defaults']['evaluate']:
+        evaluate(cfg, best_weight_path, device)
     
     #testing()  
     #test_2(cfg)
